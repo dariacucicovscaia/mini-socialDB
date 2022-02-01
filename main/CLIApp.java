c0ef8603b25b077576ef7f393e93ad52911c445f
dariacucicovscaia <91362775+dariacucicovscaia@users.noreply.github.com>
2022-02-01 11:41:36 +0300

    Update CLIApp.java

--- main/CLIApp.java
+++ main/CLIApp.java
@@ -7,7 +7,7 @@ import domain.UserDAO;
 public class CLIApp {
 
 	public static void main(String[] args) throws SQLException {
-		String url = "jdbc:postgresql://localhost/mini-social?user=postgres&password=069131467dasa";
+		String url = "jdbc:postgresql://localhost/mini-social?user=postgres&password=qazwsx";
 		String sql = "SELECT id, nickname, email, password FROM public.users;";
 		UserDAO userList = new UserDAO(url, sql);
 		for (User userlist : userList.getUsers()) {
