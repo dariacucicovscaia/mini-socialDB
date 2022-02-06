20eefd08597315d1a59afa54e561fc35cc378ef8
dariacucicovscaia <91362775+dariacucicovscaia@users.noreply.github.com>
2022-02-06 10:02:50 +0200

    Add files via upload

--- main/CLIApp.java
+++ main/CLIApp.java
@@ -1,18 +1,39 @@
 package main;
 
 import java.sql.SQLException;
+import java.util.List;
+
+import domain.Relations;
+import domain.RelationsDAO;
 import domain.User;
 import domain.UserDAO;
 
 public class CLIApp {
 
 	public static void main(String[] args) throws SQLException {
-		String url = "jdbc:postgresql://localhost/mini-social?user=postgres&password=qazwsx";
-		String sql = "SELECT id, nickname, email, password FROM public.users;";
-		UserDAO userList = new UserDAO(url, sql);
-		for (User userlist : userList.getUsers()) {
-			System.out.println(userlist.getNickname() + " " + userlist.getEmail());
+		UserDAO userList = new UserDAO();
+		RelationsDAO relationsList = new RelationsDAO();
+
+		for (Relations relationslist : relationsList.getAll()) {
+			System.out.println(relationslist.getTo_user_id());
 		}
+		System.out.println();
+
+		System.out.println(relationsList.get(1).getStatus_id());
+		//relationsList.put(new Relations(3, 2, 1, 1));
+		//relationsList.update(new Relations(3, 2, 1, 2));
+		//relationsList.remove(1);
+
+/////////////////////////////////////////////////////////////////////////////////
+//		for (User userlist : userList.getAll()) {
+//			System.out.println(userlist.getNickname() + " " + userlist.getEmail());
+//		}
+//		System.out.println();
+//		
+		// System.out.println(userList.get(1).getNickname());
+		// userList.put(new User(2, "user2", "user2@example.com", "123"));
+		// userList.update(new User(2, "user2", "user2@example.com", "546"));
+		// userList.remove(2);
 
 	}
 
